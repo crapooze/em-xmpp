c86a4648b3635d503a7dde1b3a91e0cb44143b97
crapooze <crapooze@gmail.com>
2013-02-06 23:24:36 +0100

    move dataform conversation state machine to helpers

--- lib/em-xmpp/helpers.rb
+++ lib/em-xmpp/helpers.rb
@@ -35,7 +35,74 @@ module EM::Xmpp
     def conversation(key)
       @conversations[key]
     end
-    
+
+    DataFormState = Struct.new(:form, :answers, :current_idx, :user_input) do
+      def current_field
+        form.fields[current_idx]
+      end
+
+      def current_values
+        current_field.values
+      end
+
+      def current_answer
+        answers.fields[current_idx]
+      end
+
+      def current_response_values
+        if current_answer
+          current_answer.values 
+        else
+          current_values
+        end
+      end
+    end
+
+    def dataform_conversation(initial_ctx,&blk)
+      form = initial_ctx.env['dataform']
+      answers = EM::Xmpp::Context::Contexts::Dataforms::Form.new('submit',[],nil,nil)
+      state = DataFormState.new(form, answers, 0, nil)
+
+      start_conversation(initial_ctx,:dataform,state) do |conv|
+        conv.prepare_callbacks(:start, :ask, :user_answer, :confirm, :submit, :cancel, &blk)
+        finalize = :submit
+
+        conv.callback(:start)
+
+        catch :cancel do
+          fields = state.form.fields.dup
+          cnt = fields.size
+          idx = 0
+          until (idx + 1) > cnt
+            state.current_idx = idx
+            conv.callback(:ask)
+            state.user_input = conv.delay.ctx
+            cb_action = conv.callback(:user_answer)
+            state.user_input = nil
+
+            case cb_action 
+            when :previous
+              idx -= 1
+              idx = [0,idx].max
+            when :next, :modified
+              idx += 1
+            when :repeat
+              #don't touch index
+            when :cancel
+              finalize = :cancel
+              throw :cancel
+            else
+              raise RuntimeError, "no such data-form action: #{cb_action}"
+            end
+          end
+
+          conv.callback(:confirm)
+          state.user_input = conv.delay.ctx
+        end
+        conv.callback(finalize)
+      end
+    end
+
     def build_submit_form(xml,form)
       xml.x(:xmlns => DataForms, :type => 'submit') do |x|
         form.fields.each do |field|
