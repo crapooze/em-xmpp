0dc9d4daab6f6db5ad8993bb333936d29d1e3ee6
notezen <notezen@gmail.com>
2013-04-19 08:59:54 +0400

    fix + refactoring for xml builder

--- lib/em-xmpp/xml_builder.rb
+++ lib/em-xmpp/xml_builder.rb
@@ -1,53 +1,53 @@
 require 'ox'
 module Ox
-  class Element
+  module Builder
 		# args = attributes and/or children in any order, multiple appearance is possible
 		# @overload build(name,attributes,children)
 		#   @param [String] name name of the Element
 		#   @param [Hash] attributes
 		#   @param [String|Element|Array] children text, child element or array of elements
-		def self.build(name, *args)
-			n = new(name)
+		def x(name, *args)
+			n = Element.new(name)
 
 			for arg in args
 				case arg
 				when Hash
 					arg.each { |k,v| n[k.to_s] = v }
 				when Array
-					arg.each { |c| n << c }
-				when NilClass
+					arg.each { |c| n << c if c}
 				else
-					n << arg
+					n << arg if arg
 				end
 			end
 
 			n
 		end
+
+    def x_if(condition, *args)
+      x(*args) if condition
+    end
+
 	end
 end
 
 module EM::Xmpp
 	module XmlBuilder
+    include Ox::Builder
+    
     class OutgoingStanza
+      include Ox::Builder
+      
       attr_accessor :xml,:params
 
       def initialize(*args)
-        node = Ox::Element.build(*args)
+        node = x(*args)
         @xml = Ox.dump(node)
         @params = node.attributes
       end
     end
 
-		def x(*args)
-			Ox::Element.build(*args)
-		end
-
-    def x_if(condition, *args)
-      Ox::Element.build(*args) if condition
-    end
-
     def build_xml(*args)
-			Ox.dump(Ox::Element.build(*args))
+			Ox.dump(x(*args))
     end
 	end
 end
